2c45dac63fc0913ccd5a53b96e22227a34e8c046
com088971 <com088971@gmail.com>
2026-09-13 16:49:36 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (81.60%) - LeetSync

--- 1603-running-sum-of-1d-array/running-sum-of-1d-array.cpp
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.cpp
@@ -3,11 +3,15 @@ public:
     vector<int> runningSum(vector<int>& nums) {
         vector<int>a;
         int sum=0;
-       for(int i=0;i<nums.size();i++){
-         sum+=nums[i];
-          a.push_back(sum);
+        for(int &s: nums){
+            sum+=s;
+            a.push_back(sum);
+        }
+    //    for(int i=0;i<nums.size();i++){
+    //      sum+=nums[i];
+    //       a.push_back(sum);
 
-       }
+    //    }
        return a;
     }
 };
