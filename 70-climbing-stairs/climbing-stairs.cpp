1d8fcef9c0fb3df16b7c2e204bb5e8e113f5fad2
com088971 <com088971@gmail.com>
2026-09-18 20:14:44 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (38.13%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+       if( n==0|| n==1||n==2) return n;     // no of ways to climb on n index
+        
+         vector<int>arr(n+1);
+          arr[0]==0;
+         arr[1]=1;
+         arr[2]=2;
+         for(int i=3;i<n+1;i++) arr[i]=arr[i-1]+arr[i-2];
+         return arr[n];
+    }
+};
