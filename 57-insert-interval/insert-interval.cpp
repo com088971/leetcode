367ea44c11b92c530ad5d6c194b02a8d02bf374b
Om chaudhary <com088971@gmail.com>
2026-09-22 20:36:09 +0530

    Time: 3 ms (29.04%) | Memory: 22.2 MB (12.04%) - LeetSync

---
+++ 57-insert-interval/insert-interval.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals,
+                               vector<int>& newInterval) {
+
+        vector<vector<int>> ans;
+
+        for (auto interval : intervals) {
+
+            // Case 1: Current interval is before newInterval
+            if (interval[1] < newInterval[0]) {
+                ans.push_back(interval);
+            }
+
+            // Case 2: Current interval is after newInterval
+            else if (interval[0] > newInterval[1]) {
+                ans.push_back(newInterval);
+                newInterval = interval;
+            }
+
+            // Case 3: Overlapping
+            else {
+                newInterval[0] = min(newInterval[0], interval[0]);
+                newInterval[1] = max(newInterval[1], interval[1]);
+            }
+        }
+
+        // Add remaining newInterval
+        ans.push_back(newInterval);
+
+        return ans;
+    }
+};
